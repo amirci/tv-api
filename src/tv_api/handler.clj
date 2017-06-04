c7802d7bca16930301780e3b31d9d31655033d3f
Amir Barylko <amir@barylko.com>
2017-06-04 02:13:05 -0700

    Added posters

--- src/tv_api/handler.clj
+++ src/tv_api/handler.clj
@@ -6,17 +6,21 @@
 (s/defschema TvShow
   {:name (describe s/Str "Name of the show")
    (s/optional-key :description) (describe s/Str "Summary of what the show is about")
+   :poster (describe s/Str "Link to the poster")
    :origin (describe {:country (s/enum :US :CA) :city s/Str} "Country and city where the show is from")})
 
 (def tv-shows
   [{:name "Seinfeld" 
     :description "A show about nothing" 
+    :poster "https://thetvdb.com/banners/_cache/posters/79169-5.jpg"
     :origin {:country :US :city "New York"}}
    {:name "How I met your mother" 
     :description "Legen... dary!" 
+    :poster "https://thetvdb.com/banners/_cache/posters/75760-34.jpg"
     :origin {:country :US :city "New York"}}
-   {:name "Murdoch Misteries" 
+   {:name "The Murdoch Mysteries" 
     :description "A detective in Toronto 1900" 
+    :poster "https://thetvdb.com/banners/_cache/posters/81670-1.jpg"
     :origin {:country :CA :city "Toronto"}}])
 
 (def app
