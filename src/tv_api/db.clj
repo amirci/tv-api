04435dc9d0db2a78f91d8141e4d4f32710334e68
Amir Barylko <amir@barylko.com>
2017-06-04 11:23:55 -0700

    Moved db to a different file

---
+++ src/tv_api/db.clj
@@ -0,0 +1,35 @@
+(ns tv-api.db)
+
+(def tv-shows
+  [{:name "Seinfeld" 
+    :description "A show about nothing" 
+    :tvdbid 79169
+    :site_rating 9
+    :rating :PG
+    :poster "https://thetvdb.com/banners/_cache/posters/79169-5.jpg"
+    :origin {:country :US}}
+   {:name "How I met your mother" 
+    :description "Legen... dary!" 
+    :tvdbid 79169
+    :poster "https://thetvdb.com/banners/_cache/posters/75760-34.jpg"
+    :site_rating 8
+    :rating :PG
+    :origin {:country :US}}
+   {:name "The Murdoch Mysteries" 
+    :description "A Victorian-era Toronto detective uses then-cutting edge forensic techniques to solve crimes, with the assistance of a female coroner who is also struggling for recognition in the face of tradition.  Based on the books by Maureen Jennings." 
+    :tvdbid 79169
+    :site_rating 8
+    :rating :PG
+    :poster "https://thetvdb.com/banners/_cache/posters/81670-1.jpg"
+    :origin {:country :CA}}
+   {:name "The big bang theory"
+    :description "What happens when hyperintelligent roommates Sheldon and Leonard meet Penny, a free-spirited beauty moving in next door, and realize they know next to nothing about life outside of the lab. Rounding out the crew are the smarmy Wolowitz, who thinks he's as sexy as he is brainy, and Koothrappali, who suffers from an inability to speak in the presence of a woman."
+    :site_rating 8
+    :tvdbid 80379
+    :rating :PG
+    :poster "https://thetvdb.com/banners/_cache/posters/80379-43.jpg"
+    :origin {:country :US}}])
+
+
+
+
--- src/tv_api/handler.clj
+++ src/tv_api/handler.clj
@@ -1,27 +1,25 @@
 (ns tv-api.handler
-  (:require [compojure.api.sweet :refer :all]
-            [ring.util.http-response :refer :all]
-            [schema.core :as s]))
+  (:require 
+    [compojure.api.sweet :refer :all]
+    [ring.util.http-response :refer :all]
+    [schema.core :as s]
+    [tv-api.db :as db]))
+
+(def Rating (s/constrained s/Int #(< 0 % 10)))
+
+(def AgeRating (s/enum :Y :Y7 :G :PG :14 :MA))
+
+(s/defschema Origin 
+  {:country (s/enum :US :CA)})
 
 (s/defschema TvShow
   {:name (describe s/Str "Name of the show")
    (s/optional-key :description) (describe s/Str "Summary of what the show is about")
+   :tvdbid (describe s/Int "Show id from theTVdb.com")
    :poster (describe s/Str "Link to the poster")
-   :origin (describe {:country (s/enum :US :CA) :city s/Str} "Country and city where the show is from")})
-
-(def tv-shows
-  [{:name "Seinfeld" 
-    :description "A show about nothing" 
-    :poster "https://thetvdb.com/banners/_cache/posters/79169-5.jpg"
-    :origin {:country :US :city "New York"}}
-   {:name "How I met your mother" 
-    :description "Legen... dary!" 
-    :poster "https://thetvdb.com/banners/_cache/posters/75760-34.jpg"
-    :origin {:country :US :city "New York"}}
-   {:name "The Murdoch Mysteries" 
-    :description "A detective in Toronto 1900" 
-    :poster "https://thetvdb.com/banners/_cache/posters/81670-1.jpg"
-    :origin {:country :CA :city "Toronto"}}])
+   :site_rating (describe Rating "Site Rating of the show from 1 to 10 stars")
+   :rating (describe AgeRating "Age rating recomendation for the show")
+   :origin (describe Origin "Country where the show is from")})
 
 (def app
   (api
@@ -38,6 +36,6 @@
       (GET "/tv-shows" []
         :return {:shows [TvShow]}
         :summary "Returns the list of tv shows"
-        (-> (ok {:shows tv-shows})
+        (-> (ok {:shows db/tv-shows})
             (header "Access-Control-Allow-Origin" "*"))))))
 
